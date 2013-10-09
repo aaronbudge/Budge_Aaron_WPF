9f0111b9d6bc7b44895037b2a6a5f407d6b7bacd
Aaron <aaronbudge@fullsail.edu>
2013-10-09 16:17:43 -0600

    first_and_second_video
    Following along with class video's.

--- values_and_variables/js/script.js
+++ values_and_variables/js/script.js
@@ -2,3 +2,23 @@
 
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
+
+/*
+var yearBorn = 1955;
+
+console.log("Hello World!");
+console.log(yearBorn);
+
+
+alert(yearBorn);
+ */
+
+var age;   //Declaration
+age = 35;  //Definition
+//Storage Container = Value
+
+
+var yearOfBirth = 1978;  //Defining and Declaring on the same line
+
+
+
