a2159c0cb20034b841187e3a69660bbf0f02c26f
Aaron <aaronbudge@fullsail.edu>
2013-10-09 17:52:12 -0600

    basic_expression_age_example

--- ScreencastExpressions/js/script.js
+++ ScreencastExpressions/js/script.js
@@ -1,9 +1,19 @@
 // Expressions
 
 
-//Basic Expression
+//Basic Expressions
 
+/*
 var a = 2; //Set's up are variable and defines it with 2
 var b;
 b = a + 3; //add 3 to a
 console.log(b);
+    */
+
+// Example Expressions
+
+//Age Example
+
+var yearBorn = 1978;
+var age = 2013 - yearBorn;
+console.log(age);
