b42ad97bbce0a9c4771a610db1a81d4b246d6295
Aaron <aaronbudge@fullsail.edu>
2013-10-09 16:33:56 -0600

    Arrays_video
    Following along with online class video.

--- values_and_variables/js/script.js
+++ values_and_variables/js/script.js
@@ -28,6 +28,7 @@ var yearOfBirth = 1978;  //Defining and Declaring on the same line
 
 //Strings and Boolean video
 
+/*
 var age = 38; //Number Variable
 
 var isStudent = false;  //Boolean Variable
@@ -35,5 +36,20 @@ var isStudent = false;  //Boolean Variable
 var firstName = "Kermit";
 
 var phrase = 'I don\'t know \n You\'ll have to speak with the professor.';
+*/
+
+//Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy"]; //0,1,2,3
+
+muppetNames[1] = "Rizzo";  //Change index #1 to Rizzo
+muppetNames[2] = "Fozzy";
+
+var num = 1;
+
+console.log(muppetNames[num]);  //Let's me see what's in index #1
+
+
+
 
 
