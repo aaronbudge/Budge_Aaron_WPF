c47f1ba99f6101b0b7bb282115c28535a8ded6f0
Aaron <aaronbudge@fullsail.edu>
2013-10-09 16:25:27 -0600

    strings_and_boolean_video
    Following along with online class video.

--- values_and_variables/js/script.js
+++ values_and_variables/js/script.js
@@ -3,6 +3,8 @@
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
 
+//Console.log and Alert video
+
 /*
 var yearBorn = 1955;
 
@@ -13,12 +15,25 @@ console.log(yearBorn);
 alert(yearBorn);
  */
 
+// Declaring and Defining Variables videos
+
+/*
 var age;   //Declaration
 age = 35;  //Definition
 //Storage Container = Value
 
 
 var yearOfBirth = 1978;  //Defining and Declaring on the same line
+*/
 
+//Strings and Boolean video
+
+var age = 38; //Number Variable
+
+var isStudent = false;  //Boolean Variable
+
+var firstName = "Kermit";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor.';
 
 
