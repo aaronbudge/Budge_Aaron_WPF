65b71ed7e245c6b1a575f30135eecf67883400bc
Aaron <aaronbudge@fullsail.edu>
2013-10-09 18:30:50 -0600

    concatenating_strings_example_1
    Following along with example 1 in video.

--- ScreencastExpressions/js/script.js
+++ ScreencastExpressions/js/script.js
@@ -56,11 +56,21 @@ console.log(perimeter);
 */
 
 // Using Arrays in Expressions
-
+/*
 var orangeBins = [234, 567, 883];
 
 var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
 console.log(total);
+*/
+
+// Concatenating Strings
+
+var firstName = "Aaron";
+var lastName = "Budge";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
 
 
 
