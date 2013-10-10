db802b5315f25f3835bd4cdd4bff9b48be6f8247
Aaron <aaronbudge@fullsail.edu>
2013-10-09 18:01:52 -0600

    arithmetic_operators
    Following along with online video example.

--- ScreencastExpressions/js/script.js
+++ ScreencastExpressions/js/script.js
@@ -14,6 +14,19 @@ console.log(b);
 
 //Age Example
 
+/*
 var yearBorn = 1978;
 var age = 2013 - yearBorn;
 console.log(age);
+*/
+
+//Arithmetic Operators
+
+//Area of the triangle is half the width times the height
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
+
+
+
