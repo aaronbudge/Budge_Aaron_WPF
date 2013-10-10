97ba2ab80a15673f2a26ab20663a65b463a74bfd
Aaron <aaronbudge@fullsail.edu>
2013-10-10 15:36:44 -0600

    trashRemoval
    Declare, define, and parse variable and user prompt.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,3 +7,4 @@
 
 var fightsPerMonth = parseInt(prompt("How many times per month do you argue with your spouse?"));
 var homeLate = parseInt(prompt("How many times did you stay out late this month?"));
+var trashRemoval = parseInt(prompt("How many time did you forget to take out the trash this month?"));
