7ca352bbc3c2df66f1d2a2d9bedb48321bf5cfc5
Aaron <aaronbudge@fullsail.edu>
2013-10-10 12:11:53 -0600

    loadsPerBottle_comment
    Added comment describing what this line of code does.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,4 +6,4 @@
 //Calculate how much you will spend on laundry detergent each year
 
 var detergentPrice = prompt("What do you pay for a bottle of laundry detergent?"); //Determine how much a user spends on a bottle of detergent.
-var loadsPerBottle = prompt("How many loads can your bottle of detergent wash?");
+var loadsPerBottle = prompt("How many loads can your bottle of detergent wash?"); //Determine how many loads each user specified bottle can wash.
