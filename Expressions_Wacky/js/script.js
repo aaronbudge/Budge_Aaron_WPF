1673320fd8a78a4d4d3479e7e1705be1f487f4f0
Aaron <aaronbudge@fullsail.edu>
2013-10-10 15:47:59 -0600

    comment_trashRemoval
    Description of what this line of code does.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,7 +7,7 @@
 
 var fightsPerMonth = parseInt(prompt("How many times per month do you argue with your wife?")); //Determine and parse the number of times the user argues with there wife
 var homeLate = parseInt(prompt("How many times did you stay out late this month?")); //Determine and parse the number of times the user stayed out to late
-var trashRemoval = parseInt(prompt("How many time did you forget to take out the trash this month?"));
+var trashRemoval = parseInt(prompt("How many time did you forget to take out the trash this month?")); //Determine and parse the number of times user forgot to take out the trash
 
 var bouquet = ["3 Lilly's", "3 Tulips", "3 Daisy's", "3 Roses"];
 
