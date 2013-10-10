81eb3e13cdb7e38b04250abd5633d05bbe0a53ca
Aaron <aaronbudge@fullsail.edu>
2013-10-09 18:24:24 -0600

    using_arrays
    Following along with class video example.

--- ScreencastExpressions/js/script.js
+++ ScreencastExpressions/js/script.js
@@ -48,10 +48,19 @@ var quiz4 = 80;
 
 var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
 //console.log(average);
-
+/*
 var length = 7;
 var width = 6;
 var perimeter = length * 2 + width * 2;
 console.log(perimeter);
+*/
+
+// Using Arrays in Expressions
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+
 
 
