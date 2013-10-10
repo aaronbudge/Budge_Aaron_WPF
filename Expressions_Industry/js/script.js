59756376cf36e9d68efbc03c19ad95e6eec35783
Aaron <aaronbudge@fullsail.edu>
2013-10-10 13:49:51 -0600

    comment_yealryPersonalCost
    Comment describing what this line of code does.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,7 +5,7 @@
 //Calculate how much a freelance web designer should charge per hour
 
 var yearlyBizCost = prompt("Please enter the total cost of your annual business expenses."); //Determine the total cost of the users annual business expenses
-var yearlyPersonalCost = prompt("Please enter the total cost of your annual personal expenses.");
+var yearlyPersonalCost = prompt("Please enter the total cost of your annual personal expenses."); //Determine the total annual cost of the users personal expenses
 var weeksPerYear = prompt("How many weeks per year do you intend to work?");
 var daysPerWeek = prompt("How many days per week will you work?");
 var hoursPerDay = prompt("How many hours will you work each day?");
