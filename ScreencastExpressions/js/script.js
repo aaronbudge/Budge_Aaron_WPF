16c0d4a0c32b35f4f26f394039c7b35740f0f5a1
Aaron <aaronbudge@fullsail.edu>
2013-10-09 18:19:02 -0600

    order_of_operations
    Following along with online video examples.

--- ScreencastExpressions/js/script.js
+++ ScreencastExpressions/js/script.js
@@ -32,7 +32,26 @@ console.log(area);
 
 // Modulo (used to find the remainder) designated by the % symbol
 
+/*
 var remainder = 9 % 2;
 console.log(remainder);
+*/
+
+// Order of Operations (PEMDAS)
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//The average is the sum of the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
 
 
