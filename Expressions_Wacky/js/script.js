fb3e7f29905d1745201d3b4d2c72ec645316ef00
Aaron <aaronbudge@fullsail.edu>
2013-10-10 15:37:35 -0600

    bouquet
    Declare and define array.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,3 +8,5 @@
 var fightsPerMonth = parseInt(prompt("How many times per month do you argue with your spouse?"));
 var homeLate = parseInt(prompt("How many times did you stay out late this month?"));
 var trashRemoval = parseInt(prompt("How many time did you forget to take out the trash this month?"));
+
+var bouquet = ["3 Lilly's", "3 Tulips", "3 Daisy's", "3 Roses"];
