142b67ed260d4e13e1715d8d41c0dee9be6ec7f0
Aaron <aaronbudge@fullsail.edu>
2013-10-10 12:10:59 -0600

    loadsPerBottle
    Declared and defined variable and user prompt.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,3 +6,4 @@
 //Calculate how much you will spend on laundry detergent each year
 
 var detergentPrice = prompt("What do you pay for a bottle of laundry detergent?"); //Determine how much a user spends on a bottle of detergent.
+var loadsPerBottle = prompt("How many loads can your bottle of detergent wash?");
