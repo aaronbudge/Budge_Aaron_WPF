d48a9f8689f48be9b8bbf9e0231a46a996082f20
Aaron <aaronbudge@fullsail.edu>
2013-10-10 12:14:25 -0600

    yearlyCost
    Declared and defined result variable and calculation.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -8,3 +8,5 @@
 var detergentPrice = prompt("What do you pay for a bottle of laundry detergent?"); //Determine how much a user spends on a bottle of detergent.
 var loadsPerBottle = prompt("How many loads can your bottle of detergent wash?"); //Determine how many loads each user specified bottle can wash.
 var loadsPerWeek = prompt("How many loads per week do you do?"); //Determine how many loads of laundry the user does each week.
+
+var yearlyCost = 52 * loadsPerWeek / loadsPerBottle * detergentPrice;
