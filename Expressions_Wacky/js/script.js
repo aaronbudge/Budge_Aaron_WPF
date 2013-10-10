a5fea091ebdb878419edef533cf905b65e09d470
Aaron <aaronbudge@fullsail.edu>
2013-10-10 15:36:07 -0600

    homeLate
    Decalre, define, and parse variable and user prompt.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -6,3 +6,4 @@
 //Calculate how many flowers you will have to buy for your spouse this month.
 
 var fightsPerMonth = parseInt(prompt("How many times per month do you argue with your spouse?"));
+var homeLate = parseInt(prompt("How many times did you stay out late this month?"));
