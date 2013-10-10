38d8baf14e01f49bc8022048c960548f50eb62c6
Aaron <aaronbudge@fullsail.edu>
2013-10-09 22:08:40 -0600

    dog_years
    Code for Dog Year problem.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -3,3 +3,14 @@
 // Expressions Worksheet
 
 
+//Dog Years
+
+//Calculate how old Sparky is in dog years based on his actual age
+var actualAge = 5; //Declare and define given variable
+
+var dogAge = actualAge * 7; //Declare and define result variable
+//Print out result of age in dog years
+console.log("Sparky is" + " " + actualAge + " " + "human years old which is" + " " + dogAge + " " + "in dog years");
+
+
+
