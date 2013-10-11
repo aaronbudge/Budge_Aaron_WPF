e02d65c5f5d4dbd344383b9afe8385e75aefe3e2
Aaron <aaronbudge@fullsail.edu>
2013-10-11 13:11:35 -0600

    parse
    Parsed strings to allow for calculation of totalExpenses variable.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,14 +4,14 @@
 
 //Calculate how much a freelance web designer should charge per hour
 
-var yearlyBizCost = prompt("Please enter the total cost of your annual business expenses."); //Determine the total cost of the users annual business expenses
-var yearlyPersonalCost = prompt("Please enter the total cost of your annual personal expenses."); //Determine the total annual cost of the users personal expenses
-var profitGoal = prompt("How much profit would you like to earn annually?"); //Determine the amount of profit the user would like to earn
+var yearlyBizCost = parseInt(prompt("Please enter the total cost of your annual business expenses.")); //Determine and parse the total cost of the users annual business expenses
+var yearlyPersonalCost = parseInt(prompt("Please enter the total cost of your annual personal expenses.")); //Determine and parse the total annual cost of the users personal expenses
+var profitGoal = parseInt(prompt("How much profit would you like to earn annually?")); //Determine and parse the amount of profit the user would like to earn
 var weeksPerYear = prompt("How many weeks per year do you intend to work?"); //Determine how many weeks per year the user works
 var daysPerWeek = prompt("How many days per week will you work?"); //Determine how many days per week the user works
 var hoursPerDay = prompt("How many hours will you work each day?"); //Determine how many hours per day the user works.
 
-var totalExpenses = Number(yearlyBizCost) + Number(yearlyPersonalCost) + Number(profitGoal);//Calculation totalling personal expenses, business expenses, and intended profit
+var totalExpenses = yearlyBizCost + yearlyPersonalCost + profitGoal;//Calculation totalling personal expenses, business expenses, and intended profit
 var billableHours = weeksPerYear * daysPerWeek * hoursPerDay; //Calculation to determine the amount of billable hours
 
 var hourlyRate = totalExpenses / billableHours; //Calculation of hourly rate based on total expenses divided by billable hours
