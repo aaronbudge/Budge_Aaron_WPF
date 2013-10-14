ed9326602f6721b9008e0705585ebfde8ee6aa74
Aaron <aaronbudge@fullsail.edu>
2013-10-14 15:34:29 -0600

    ternary_opertators_example_2
    Following along with video example.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -89,11 +89,14 @@ if(iPhonePrice < budget || wonLottery){
 var gpa = 48;
 
 //If the gpa is over the min 2.0 score, the student can graduate
-if(gpa > 2.0){
+/*if(gpa > 2.0){
     console/log("You can graduate!");
 }else{
     console.log("GPA is to low!");
-}
+}*/
+
+(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is to low!")
+
 
 
 
