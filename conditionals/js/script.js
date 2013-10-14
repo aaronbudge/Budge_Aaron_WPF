6079cb2afa81dd50a6f84de5fa3461a1d0401d10
Aaron <aaronbudge@fullsail.edu>
2013-10-14 15:18:33 -0600

    logical_operators_and
    Following along with example.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -69,3 +69,18 @@ if(kidHeight > minHeight){
     //Sorry you have growing to do
     console.log("Sorry kid, you've got some growing to do first!");
 }
+
+
+// Logical Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+//If the price of the phone is less than our budget And if our paycheck is over 300.00
+if(iPhonePrice < budget && paycheck > 300){
+    console.log("We can buy the phone!");
+}else {
+    console.log("No phone for you!!");
+}
+
