e3bb52595727fbc4200e65c3c863290a43ebea96
Aaron <aaronbudge@fullsail.edu>
2013-10-14 14:27:39 -0600

    elas_if
    Following along with video example.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -51,3 +51,21 @@ if(kidHeight > minHeight){
 }else{
     console.log("Sorry kid, you've got some growing to do first!");
 }
+
+// else if
+
+var kidHeight = 52;
+var minHeight = 48;
+var wParentHeight = 45; //The height of the kid with parent
+
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //You can ride
+    console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //You can ride with a parent present
+    console.log("You can ride, but only with a parent present!");
+}else{
+    //Sorry you have growing to do
+    console.log("Sorry kid, you've got some growing to do first!");
+}
