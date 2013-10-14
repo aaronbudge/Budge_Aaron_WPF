dfd2e36d814f93b96f1e2867082dd9c7205ad76d
Aaron <aaronbudge@fullsail.edu>
2013-10-14 14:17:04 -0600

    if_and_else
    Following along with video example.

--- conditionals/js/script.js
+++ conditionals/js/script.js
@@ -39,3 +39,15 @@ if(kidHeight + sneakerLift > minHeight){
 }
 
 
+// if and else
+
+var kidHeight = 52;
+var minHeight = 48;
+
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}else{
+    console.log("Sorry kid, you've got some growing to do first!");
+}
