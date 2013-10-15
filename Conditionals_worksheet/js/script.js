3945b7ff76f1f94c7cfa08f26c1a3fcad7227fcf
Aaron <aaronbudge@fullsail.edu>
2013-10-15 14:39:05 -0600

    lastChance_if_condition
    Declared and defined if condition.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -13,3 +13,7 @@ var tankCapacity = 25; //Initialize how much gas the gas tank will hold
 
 var gasInTank = tankCapacity * tankLevel; //Calculate how much gas is in the tank
 var milesTillEmpty = gasInTank * mpg; //Calculate how many miles the car can go before running out of gas
+
+if(milesTillEmpty > 200){
+    console.log("Yes, you can make it without stopping for gas!");
+}
