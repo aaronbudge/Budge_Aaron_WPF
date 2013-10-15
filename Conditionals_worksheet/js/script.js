b63a4fbceacbd111ee55ccdaa81989d9ebebb92b
Aaron <aaronbudge@fullsail.edu>
2013-10-15 15:16:42 -0600

    closed_project

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -46,7 +46,7 @@ if(username === accountName && password != accountPassword){ //Test multiple con
 //Movie Ticket Price
 //What price is the customer eligible for?
 var movieTime = 5; //Declare and initialize the movieTime variable
-var customerAge = 50; //Declare and initialize the customerAge variable
+var customerAge = 55; //Declare and initialize the customerAge variable
 
 if((customerAge >= 55 || customerAge) < 10 || (movieTime > 3 && movieTime < 5)){ //Test multiple conditions
     console.log("The ticket price is $7.00."); //Print to console if condition is true
