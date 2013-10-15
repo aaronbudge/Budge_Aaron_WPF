b9b1b2a54c396a8617dbd77a8217bfaa85fb0560
Aaron <aaronbudge@fullsail.edu>
2013-10-15 14:32:50 -0600

    comment_tankCapacity
    Description of what this line of code does

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -9,4 +9,4 @@
 //Can you make it across the desert with your current fuel?
 var mpg = 17; //Initialize gas efficiency in miles per gallon
 var tankLevel = .35; //Initialize the reading from the gas gauge
-var tankCapacity = 25; 
+var tankCapacity = 25; //Initialize how much gas the gas tank will hold
