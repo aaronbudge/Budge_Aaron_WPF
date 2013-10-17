3c858904fe76efcad0cf3ae5fc456533950e906e
Aaron <aaronbudge@fullsail.edu>
2013-10-17 10:25:44 -0600

    var_waterFlow
    Declare and initiate prompt for the waterFlow variable.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,3 +4,4 @@
 
 //How many people can shower before the hot water runs out?
 var waterTank = parseInt(prompt("How many liters of water does your tank hold?")); //Declare variable and initiate user prompt to determine water tank size
+var waterFlow = parseInt(prompt("How many liters of water move through your shower head per minute?"));
