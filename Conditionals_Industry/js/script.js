a64459dab3be93198f04fce30eae5c84499b7189
Aaron <aaronbudge@fullsail.edu>
2013-10-17 12:22:32 -0600

    comment_pages
    Description of what this line of code does.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,7 +5,7 @@
 // Web Design Cost Calculator
 
 // How much will it cost to design and develop your website?
-var pages = prompt("How many pages will your site have?");
+var pages = prompt("How many pages will your site have?"); //Declare variable and initiate user prompt
 
 var codingBase = 15; //Declare and initiate the base hours needed for page coding
 
