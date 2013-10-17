257b182c29e71ff72e6538e6d03be28ae549b7ff
Aaron <aaronbudge@fullsail.edu>
2013-10-17 13:10:51 -0600

    changed_variables
    Changed and rearanged variables to better reflect calculation.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,9 +6,9 @@
 
 // How much will it cost to design and develop a basic website?
 var pages = prompt("How many pages will your site have?"); //Declare variable and initiate user prompt
-var design = prompt("How many pages will have their own design?"); //Declare variable and initiate user prompt
-
-var codingBase = 15; //Declare and initiate the base hours needed for page coding
-var designBase = 20; //Declare, define, and initialize the base hours needed for page design
+var design = prompt("How many pages will have a different design than the homepage?"); //Declare variable and initiate user prompt
 var addPage = 3; //Declare, define, and initialize the hours needed to code each additional page
 var addDesign = 4; //Declare, define, and initialize the hours needed to design each additional page design
+
+var codingHrs = 15 + pages * addPage; //Declare and initiate the total hours needed for page coding
+var designHrs = 20 + design * addDesign; //Declare, define, and initialize the total hours needed for page design
