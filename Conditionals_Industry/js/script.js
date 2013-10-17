b4303f9ef5c69f210808534c9908c7a4bf8b6398
Aaron <aaronbudge@fullsail.edu>
2013-10-17 13:15:56 -0600

    comment_hourlyRate
    Description of what this line of code does.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,7 +9,7 @@ var pages = prompt("How many pages will your site have?"); //Declare variable an
 var design = prompt("How many pages will have a different design than the homepage?"); //Declare variable and initiate user prompt
 var addPage = 3; //Declare, define, and initialize the hours needed to code each additional page
 var addDesign = 4; //Declare, define, and initialize the hours needed to design each additional page design
-var hourlyRate = 25;
+var hourlyRate = 25; //Declare, define and initialize hourly rate 
 
 pages--; //Decreased page amount by 1 to account for homepage included in base coding hours (15)
 
