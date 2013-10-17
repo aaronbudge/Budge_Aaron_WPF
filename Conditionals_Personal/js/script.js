5c0512b81924fef2f1b9fe6fce714992f363880a
Aaron <aaronbudge@fullsail.edu>
2013-10-17 10:35:54 -0600

    var_hotWaterTime
    Declare, calculate and define the hotWaterTime variable.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,3 +6,5 @@
 var waterTank = parseInt(prompt("How many liters of water does your tank hold?")); //Declare variable and initiate user prompt to determine water tank size
 var waterFlow = parseInt(prompt("How many liters of water move through your shower head per minute?")); //Declare variable and initiate user prompt to determine water flow in liters per minute
 var showerTime = parseInt(prompt("Enter the average time each person will shower?")); //Declare variable and initiate prompt to determine the per person shower time
+
+var hotWaterTime = waterTank / waterFlow;
