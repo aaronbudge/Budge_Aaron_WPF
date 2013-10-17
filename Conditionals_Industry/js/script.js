7cc942cb022d9e31a3a009eea1b8608eba9d03e3
Aaron <aaronbudge@fullsail.edu>
2013-10-17 12:25:47 -0600

    var_design
    Declare variable and intiate user rpompt.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,6 +6,7 @@
 
 // How much will it cost to design and develop your website?
 var pages = prompt("How many pages will your site have?"); //Declare variable and initiate user prompt
+var design = prompt("How many pages will have their own design?");
 
 var codingBase = 15; //Declare and initiate the base hours needed for page coding
 
