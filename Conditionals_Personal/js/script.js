9c319eebef189cb351252efa3b87a2884430684c
Aaron <aaronbudge@fullsail.edu>
2013-10-17 11:10:29 -0600

    clean_up
    Small adjustments to verbiage.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,7 +2,7 @@
 // October 16, 2013
 // Conditionals_Personal
 
-//How many people can shower before the hot water runs out?
+//How many people can complete their shower before the hot water runs out?
 var waterTank = parseInt(prompt("How many liters of water does your tank hold?")); //Declare variable and initiate user prompt to determine water tank size
 var waterFlow = parseInt(prompt("How many liters of water move through your shower head per minute?")); //Declare variable and initiate user prompt to determine water flow in liters per minute
 var showerTime = parseInt(prompt("Enter the average time each person will shower?")); //Declare variable and initiate prompt to determine the per person shower time
@@ -14,5 +14,5 @@ var totalPeople = hotWaterTime / showerTime; //Declare, calculate and define var
 if(hotWaterTime <= showerTime){ //Test condition
     console.log("One person will be able to shower for " + hotWaterTime + " minutes, before the water turns cold."); //Print out result to console log if the condition is true
 }else{
-    console.log(parseInt(totalPeople) + " people will be able to shower before the water turns cold."); //Print out to console log if the condition is false
+    console.log(parseInt(totalPeople) + " people will be able to complete their shower before the water turns cold."); //Print out to console log if the condition is false
 }
