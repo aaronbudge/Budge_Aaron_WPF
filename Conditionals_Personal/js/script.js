1bd45bdc8279a68e33262945a15987f7c469d59c
Aaron <aaronbudge@fullsail.edu>
2013-10-17 10:30:14 -0600

    comment_showerTime
    Description of what this line of code does.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,4 +5,4 @@
 //How many people can shower before the hot water runs out?
 var waterTank = parseInt(prompt("How many liters of water does your tank hold?")); //Declare variable and initiate user prompt to determine water tank size
 var waterFlow = parseInt(prompt("How many liters of water move through your shower head per minute?")); //Declare variable and initiate user prompt to determine water flow in liters per minute
-var showerTime = parseInt(prompt("Enter the average time each person will shower?"));
+var showerTime = parseInt(prompt("Enter the average time each person will shower?")); //Declare variable and initiate prompt to determine the per person shower time
