d7bc7435fb7c64b3c54031aab628ee8ad7426003
Aaron <aaronbudge@fullsail.edu>
2013-10-17 12:26:26 -0600

    comment_design
    Description of what this line of code does.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,7 +6,7 @@
 
 // How much will it cost to design and develop your website?
 var pages = prompt("How many pages will your site have?"); //Declare variable and initiate user prompt
-var design = prompt("How many pages will have their own design?");
+var design = prompt("How many pages will have their own design?"); //Declare variable and initiate user prompt
 
 var codingBase = 15; //Declare and initiate the base hours needed for page coding
 
