40dbfa1c776209e0f47114d1215a44f8cefcb9d4
Aaron <aaronbudge@fullsail.edu>
2013-10-17 12:33:01 -0600

    var_designBase
    Declare, define, and intitialize variable.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,9 +4,9 @@
 
 // Web Design Cost Calculator
 
-// How much will it cost to design and develop your website?
+// How much will it cost to design and develop a basic website?
 var pages = prompt("How many pages will your site have?"); //Declare variable and initiate user prompt
 var design = prompt("How many pages will have their own design?"); //Declare variable and initiate user prompt
 
 var codingBase = 15; //Declare and initiate the base hours needed for page coding
-
+var designBase = 20;
