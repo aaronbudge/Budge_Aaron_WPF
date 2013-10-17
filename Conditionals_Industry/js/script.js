6afc6bcf7acc6bb8b791909267fe6bcdb9409160
Aaron <aaronbudge@fullsail.edu>
2013-10-17 13:28:14 -0600

    var_projectCost
    Declare and calculate the projectCost variable.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -17,4 +17,8 @@ var codingHrs = 15 + pages * addPage; //Declare and initiate the total hours nee
 var designHrs = 20 + design * addDesign; //Declare, define, and initialize the total hours needed for page design
 var totalHours = codingHrs + designHrs; //Declare, calculate and define the total hours for project
 
+var projectCost = totalHours * hourlyRate;
+
+
+
 
