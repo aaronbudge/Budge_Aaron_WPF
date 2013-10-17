83de3857f9c8a2fd320dd5a787dd32d4a22f3341
Aaron <aaronbudge@fullsail.edu>
2013-10-17 13:15:05 -0600

    var_hourlyRate
    Declared, defined and intiated the hourlyRate variable.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -9,8 +9,11 @@ var pages = prompt("How many pages will your site have?"); //Declare variable an
 var design = prompt("How many pages will have a different design than the homepage?"); //Declare variable and initiate user prompt
 var addPage = 3; //Declare, define, and initialize the hours needed to code each additional page
 var addDesign = 4; //Declare, define, and initialize the hours needed to design each additional page design
+var hourlyRate = 25;
 
 pages--; //Decreased page amount by 1 to account for homepage included in base coding hours (15)
 
 var codingHrs = 15 + pages * addPage; //Declare and initiate the total hours needed for page coding
 var designHrs = 20 + design * addDesign; //Declare, define, and initialize the total hours needed for page design
+
+
