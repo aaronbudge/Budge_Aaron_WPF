3a7ed99a23f076c4dfc585e0ab6edd51a47dd3f9
Aaron <aaronbudge@fullsail.edu>
2013-10-17 17:53:38 -0600

    2nd_else_if
    Set condition to test.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -17,4 +17,5 @@ if(dateType === "a" && kids < 5){//Test condition
 }else if(dateType === "b" && kids < 5){//Test condition
     var odds = 85 / kids; //Declare and define variable if condition is true
     console.log("You have a \%" + parseInt(odds) + " chance of getting lucky tonight!");//Print out to console log if condition is true
-}
+}else if(dateType === "c" && kids < 5){
+    
