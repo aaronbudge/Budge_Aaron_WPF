944b8d2661a0e11a93849efe73690740d9ece008
Aaron <aaronbudge@fullsail.edu>
2013-10-17 18:04:22 -0600

    comments_elseif_3
    Description of what these lines of code do.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -18,10 +18,10 @@ if(dateType === "a" && kids < 5){//Test condition
     var odds = 85 / kids; //Declare and define variable if condition is true
     console.log("You have a \%" + parseInt(odds) + " chance of getting lucky tonight!");//Print out to console log if condition is true
 }else if(dateType === "c" && kids < 5){ //Condition to test
-    var drinks = prompt("How many drinks will your spouse have?"); //Variable to declare and define if previous conditions are false
+    var drinks = prompt("How many drinks will your spouse have?"); //Variable to declare and define if previous conditions are false.
     if(drinks >= 4 && kids < 5){ //Conditions to test
         console.log("Forget the movie and head home, you're getting lucky tonight!"); //Result to print out to console log if conditions are true.
-    }else if(kids > 1 && drinks <= 3){
-        var odds = 50 / kids * drinks;
-        console.log("You have a \%" + parseInt(odds) + " chance of getting lucky tonight!");
+    }else if(kids > 1 && drinks <= 3){ //Conditions to test.
+        var odds = 50 / kids * drinks; //Variable to declare and define if conditions are true.
+        console.log("You have a \%" + parseInt(odds) + " chance of getting lucky tonight!"); //Result to print out if conditions are true.
     }
