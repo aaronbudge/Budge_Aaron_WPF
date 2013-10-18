d498c6843b4e68467b0aad7ae3ef4a4a4536bc30
Aaron <aaronbudge@fullsail.edu>
2013-10-17 20:02:36 -0600

    var_codingCost
    Declare, define and calculate variable

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,8 +5,9 @@
 // Web Design Cost Calculator
 
 // How much will it cost to design and develop a basic website?
-var pages = prompt("How many pages will your site have?"); //Declare variable and initiate user prompt
-var design = prompt("How many pages will have a different design than the homepage?"); //Declare variable and initiate user prompt
+var pages = parseInt(prompt("How many pages will your site have?")); //Declare variable and initiate user prompt
+var design = parseInt(prompt("How many pages will have a different design than the homepage?")); //Declare variable and initiate user prompt
+
 var addPage = 3; //Declare, define, and initialize the hours needed to code each additional page
 var addDesign = 4; //Declare, define, and initialize the hours needed to design each additional page design
 var hourlyRate = 25; //Declare, define and initialize hourly rate
@@ -17,7 +18,9 @@ var codingHrs = 15 + pages * addPage; //Declare and initiate the total hours nee
 var designHrs = 20 + design * addDesign; //Declare, define, and initialize the total hours needed for page design
 var totalHours = codingHrs + designHrs; //Declare, calculate and define the total hours for project
 
+var codingCost = codingHrs * hourlyRate;
 var projectCost = totalHours * hourlyRate; //Declare, define and calculate the total amount billed to the customer
+
 console.log("We can design and develop your site for $" + projectCost + " dollars."); //Print out result to console.log
 
 
