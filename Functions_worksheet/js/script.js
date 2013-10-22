530c4800afe3e2ed46446d74b4d93dae96ee58bd
Aaron <aaronbudge@fullsail.edu>
2013-10-22 17:46:12 -0600

    function_calcBeeStings
    Definition and return of the calcBeeStings function.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -21,3 +21,7 @@ function calculateCirc(r){ //Code to be performed when called
 var vicWeight = 200; //Declared and defined variable
 var beeStings = calcBeeStings(vicWeight); //Declared result variable and argument for function call
 console.log("It takes " + beeStings + " bee stings to kill this animal."); //Print result to console log after function is performed and calculation is returned to the result variable.
+
+function calcBeeStings(lbs){
+    return 8.666666667 * lbs;
+}
