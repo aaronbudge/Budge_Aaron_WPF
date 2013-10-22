698b9b971941c5b482ada1ceb34da79093bf78e5
Aaron <aaronbudge@fullsail.edu>
2013-10-22 12:59:11 -0600

    functions_variable_scope
    Following along with online video.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -15,7 +15,7 @@ function calcArea (){ //Code block to execute when called
 
 //Functions - Function Execution
 
-function outputMsg (){ //Code block to execute when called
+/*function outputMsg (){ //Code block to execute when called
     console.log("Hello World"); //Print to console when function is called
 }
 
@@ -28,5 +28,22 @@ function calcArea (){ //Code block to execute when called
 
 calcArea(); //Calling the calcArea function
 calcArea(); //Calling the calcArea function
+calcArea(); //Calling the calcArea function*/
+
+//Functions - Variable Scope
+
+var width = 5; //Global variable
+
+ function outputMsg (){ //Code block to execute when called
+    console.log("Hello World"); //Print to console when function is called
+ }
+
+ function calcArea (){ //Code block to execute when called
+     var width = 20;//Declare and define when function is called
+     var height = 30; //Declare and define when function is called
+     var area = width * height; //Declare and define when function is called
+     console.log(area); //Print to console when function is called
+ }
 calcArea(); //Calling the calcArea function
+console.log(width);
 
