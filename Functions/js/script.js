b7897e355e6dd6d8156b698be9a5ce57d1aa9d18
Aaron <aaronbudge@fullsail.edu>
2013-10-22 12:50:06 -0600

    function_basic_structures
    Following along with online video.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -1,1 +1,14 @@
-// JavaScript
+// Aaron Budge
+
+//Functions - Basic Structures
+
+function outputMsg (){ //Code block to execute when called
+    console.log("Hello World"); //Print to console when function is called
+}
+
+function calcArea (){ //Code block to execute when called
+    var width = 20;//Declare and define when function is called
+    var height = 30; //Declare and define when function is called
+    var area = width * height; //Declare and define when function is called
+    console.log(area); //Print to console when function is called
+}
