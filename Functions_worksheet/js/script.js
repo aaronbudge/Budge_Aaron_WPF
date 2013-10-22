aed0de2ac6d4d4c0ddda16110b1a03baae8fa672
Aaron <aaronbudge@fullsail.edu>
2013-10-22 17:24:56 -0600

    comment_circ_console_log
    Description of what this line of code does.

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,4 +8,4 @@
 
 var radius = 6; //Declared and defined variable
 var circumference = calculateCirc(radius); //Declared result variable for function call using an argument
-console.log("The circumference of the circle is " + circumference + " inches.");
+console.log("The circumference of the circle is " + circumference + " inches."); //Print result to console log after function is performed and returned to the result variable.
