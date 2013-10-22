03d7b65ed34f912f100a1e82850922d597dbb845
Aaron <aaronbudge@fullsail.edu>
2013-10-22 12:53:56 -0600

    function_execution
    Following along with online video.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -2,6 +2,19 @@
 
 //Functions - Basic Structures
 
+/*function outputMsg (){ //Code block to execute when called
+    console.log("Hello World"); //Print to console when function is called
+}
+
+function calcArea (){ //Code block to execute when called
+    var width = 20;//Declare and define when function is called
+    var height = 30; //Declare and define when function is called
+    var area = width * height; //Declare and define when function is called
+    console.log(area); //Print to console when function is called
+}*/
+
+//Functions - Function Execution
+
 function outputMsg (){ //Code block to execute when called
     console.log("Hello World"); //Print to console when function is called
 }
@@ -12,3 +25,8 @@ function calcArea (){ //Code block to execute when called
     var area = width * height; //Declare and define when function is called
     console.log(area); //Print to console when function is called
 }
+
+calcArea(); //Calling the calcArea function
+calcArea(); //Calling the calcArea function
+calcArea(); //Calling the calcArea function
+
