6ca5821528d57729b88a6b1f6058d0322ebce5cd
Aaron <aaronbudge@fullsail.edu>
2013-10-23 16:01:36 -0600

    function_calcPercent
    Changed parameter names to be clearer.

--- Functions_industry/js/script.js
+++ Functions_industry/js/script.js
@@ -6,6 +6,6 @@
 var totalWidth = prompt("Please enter the total available width in pixels."); //Declare variable and initiate user prompt to determine total width
 var target = prompt("Please enter your targeted width in pixels."); //Declare variable and initiate user prompt to determine targeted width
 
-function calcPercent(t, c){ //Declare and define function
-    return t / c * 100; //Code to be calculated and returned when the function is called
+function calcPercent(target, total){ //Declare and define function
+    return target / total * 100; //Code to be calculated and returned when the function is called
 }
