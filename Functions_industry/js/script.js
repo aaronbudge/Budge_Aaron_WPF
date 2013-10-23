410c31616e6dbc9282ac57ea34815eb5a70e8267
Aaron <aaronbudge@fullsail.edu>
2013-10-23 16:05:25 -0600

    1st_if_statement
    Set conditions for the first if statement.

--- Functions_industry/js/script.js
+++ Functions_industry/js/script.js
@@ -11,3 +11,7 @@ function calcPercent(target, total){ //Declare and define function
 }
 
 var percent = calcPercent(target, totalWidth); //Declared result variable, called and passed arguments to calcPercent function
+
+if(totalWidth === "" && target === ""){
+    
+}
