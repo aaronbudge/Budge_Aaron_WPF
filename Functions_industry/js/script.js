ee26743c186e7657e37bc21be056ad0ec6f191b9
Aaron <aaronbudge@fullsail.edu>
2013-10-23 15:53:04 -0600

    var_totalWidth
    Decalred variable and initialized user prompt.

--- Functions_industry/js/script.js
+++ Functions_industry/js/script.js
@@ -3,3 +3,4 @@
 // Functions_industry
 
 //Convert a fixed width layout to a fluid width layout by converting pixels to percentages
+var totalWidth = prompt("Please enter the total available width in pixels.");
