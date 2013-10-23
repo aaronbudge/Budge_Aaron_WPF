7d60982bad1bec7966c84d515808ea6b79f1fa23
Aaron <aaronbudge@fullsail.edu>
2013-10-23 14:28:00 -0600

    comment_anonymous_function_calGallons
    Descriptio of what these lines of code do.

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -8,6 +8,6 @@ var tubLength = prompt("Please enter the interior length of your tub, in inches.
 var tubWidth = prompt("Please enter the interior width of your tub, in inches."); //Declare variable and initialize user prompt to determine tub width
 var tubDepth = prompt("How many inches are between the bottom of your tub and the tubs overflow spout?"); //Declare variable and initialize user prompt to determine tub depth
 
-var calcGallons = function(l, w, d){
-    return l * w * d / 231;
+var calcGallons = function(l, w, d){ //Declare and define anonymous function
+    return l * w * d / 231; //Code to calculate and return when function is called
 }
