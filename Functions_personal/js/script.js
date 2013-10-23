39aac46b88c50744c2bc9945e2208649e1b1f4f4
Aaron <aaronbudge@fullsail.edu>
2013-10-23 14:23:45 -0600

    var_tubDepth
    Declare variable and initiate user prompt.

--- Functions_personal/js/script.js
+++ Functions_personal/js/script.js
@@ -6,3 +6,4 @@
 
 var tubLength = prompt("Please enter the interior length of your tub, in inches."); //Declare variable and initialize user prompt to determine tub length
 var tubWidth = prompt("Please enter the interior width of your tub, in inches."); //Declare variable and initialize user prompt to determine tub width
+var tubDepth = prompt("How many inches are between the bottom of your tub and the tubs overflow spout?");
