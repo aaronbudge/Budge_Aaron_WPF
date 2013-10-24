267d27329639d012690c4af70c3800daaa78cbc8
Aaron <aaronbudge@fullsail.edu>
2013-10-24 14:15:32 -0600

    comment_var_aging
    Description of what this line of code does.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,4 +5,4 @@
 // Calculate the chances your neighbors are Vampires
 
 var skin = prompt("On a scale of 1 - 10, how pale is their skin?"); //Declared variable and initialized user prompt to rate paleness of skin
-var aging = prompt("On a scale of 1 - 10, rate their lack of aging.");
+var aging = prompt("On a scale of 1 - 10, rate their lack of aging."); //Declared variable and initialized user prompt to rate signs of aging
