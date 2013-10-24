95fae3c193d33d690437042d09138033bdb28715
Aaron <aaronbudge@fullsail.edu>
2013-10-24 14:19:53 -0600

    comment_function_calcVampOdds
    Description of what these lines of code do.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -8,6 +8,6 @@ var skin = prompt("On a scale of 1 - 10, how pale is their skin?"); //Declared v
 var aging = prompt("On a scale of 1 - 10, rate their lack of aging."); //Declared variable and initialized user prompt to rate signs of aging
 var sunlight = prompt("Have you ever seen them in sunlight?\n\(a\) Yes\n\(b\) No"); //Declared variable and initialized user prompt
 
-function calcVampOdds(s, a){
-    return s * a;
+function calcVampOdds(s, a){ //Named and initialized function and set parameters
+    return s * a; //Code to perform and return when function is called
 }
