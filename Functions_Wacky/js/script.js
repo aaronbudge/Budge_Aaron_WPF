5342bae2589dcb7c29b240f4febe7aea619c6d5a
Aaron <aaronbudge@fullsail.edu>
2013-10-24 14:17:28 -0600

    comment_var_sunlight
    Description of what this line of code does.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -6,4 +6,4 @@
 
 var skin = prompt("On a scale of 1 - 10, how pale is their skin?"); //Declared variable and initialized user prompt to rate paleness of skin
 var aging = prompt("On a scale of 1 - 10, rate their lack of aging."); //Declared variable and initialized user prompt to rate signs of aging
-var sunlight = prompt("Have you ever seen them in sunlight?\n\(a\) Yes\n\(b\) No");
+var sunlight = prompt("Have you ever seen them in sunlight?\n\(a\) Yes\n\(b\) No"); //Declared variable and initialized user prompt
